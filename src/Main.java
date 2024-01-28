6c81efb0db45652adaa3be86e14e9d5310aa465b
Kauan Jasiskis <jasiskiskauan@gmail.com>
2024-01-28 11:18:54 -0300

    Adicao de dica para funcionamento do programa

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
+        //para o programa funcionar corretamente troque o caminho do arquivo para onde voce deseja que ele seja criado.
         File arquivo = new File("C:\\Users\\User\\IdeaProjects\\Projeto2\\Agenda.txt");
         ManipulacaoDeArquivo.criarArquivo(arquivo);
         Menu.apresentaOperaMenu(arquivo,sc);
